61c55c05eb6779ee7d1ef75cf265768079b601a8
adamdoha <adamdoha@naver.com>
2021-02-25 00:39:50 +0900

    feat : DeciderJob

--- src/main/java/me/dolphago/job/StepNextConditionalJobConfiguration.java
+++ src/main/java/me/dolphago/job/StepNextConditionalJobConfiguration.java
@@ -21,15 +21,15 @@ public class StepNextConditionalJobConfiguration {
     @Bean
     public Job stepNextConditionalJob() {
         return jobBuilderFactory.get("stepNextConditionalJob")
-                                .start(conditionalJobStep1())
+                                .start(conditionalJobStep1())// start대신 flow를 사용해도 된다.
                                 .on("FAILED")// Step1이 FAILED일 경우
                                 .to(conditionalJobStep3())// Step3으로 이동한다.
                                 .on("*")// Step3의 결과 관계 없이
                                 .end()//Step3으로 이동하면 Flow가 종료된다.
                                 .from(conditionalJobStep1())//Step1로부터
                                 .on("*")//FAILED 외의 모든 경우
-                                .to(conditionalJobStep2())// Step 2로 이동한다.
-                                .next(conditionalJobStep3())//Step 2가 정상 종료하면, Step3으로 이동한다.
+                                .to(conditionalJobStep3())// Step 2로 이동한다.
+                                .next(conditionalJobStep2())//Step 2가 정상 종료하면, Step3으로 이동한다.
                                 .on("*")//Step 3의 결과와 관계 없이
                                 .end()
                                 .end()//Job 종료
